41c533c89fe60a141416a965c7cb0b210a21024a
joshgermon <joshgermon@gmail.com>
2021-06-21 22:36:52 +1000

    Update: Added colours

--- nbplay.py
+++ nbplay.py
@@ -3,6 +3,22 @@ import time
 from datetime import datetime
 import pytz
 
+# Styles class
+
+
+class style:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+
 # Define Variables
 base_url = 'http://data.nba.net'
 
@@ -17,7 +33,7 @@ def get_fmt_date():
 
 
 # Test date
-test_date = '20210617'
+test_date = '20210620'
 
 # Get Play by Play
 
@@ -33,8 +49,8 @@ def show_plays(gameId):
     while active_game == 1:
         play_data = get_play_data(gameId)
         plays = play_data['sports_content']['game']['play']
-        current_play = '{}: {}'.format(
-            plays[200]['clock'], plays[200]['description'])
+        current_play = '{}{}:{} {}'.format(
+            style.RED, plays[200]['clock'], style.RESET, plays[200]['description'])
         print(current_play, end="\r")
         time.sleep(3)
 
@@ -42,13 +58,13 @@ def show_plays(gameId):
 
 
 def show_games_today(games_today):
-    print('Todays games:')
+    print(style.MAGENTA + 'Todays games:' + style.RESET)
     # Loop over games
     for game in games_today:
         print('{} @ {}'.format(game['hTeam']
               ['triCode'], game['vTeam']['triCode']))
 
-    selected_game = input('What game? ')
+    selected_game = input(style.GREEN + 'What game? ' + style.RESET)
 
     for game in games_today:
         if selected_game == game['hTeam']['triCode']:
